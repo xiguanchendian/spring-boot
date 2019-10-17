c133baecac3fc3c837412efb51265095bcd43292
liangyadong <17600665028@163.com>
2019-10-17 22:05:30 +0800

    @ImportResource 导入spring的配置文件, 让配置文件里面的内容生效,像@SpringBootApplication一样加在Application.java上.

--- src/main/java/com/xgcd/springboot/Application.java
+++ src/main/java/com/xgcd/springboot/Application.java
@@ -2,10 +2,13 @@ package com.xgcd.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
- * @SpringBootApplication 该注解用于标注一个主程序类,说明这是一个springboot应用
+ * @SpringBootApplication 该注解用于标注一个主程序类, 说明这是一个springboot应用
+ * @ImportResource 导入spring的配置文件, 让配置文件里面的内容生效
  */
+//@ImportResource(locations = {"classpath:spring-beans.xml"})
 @SpringBootApplication
 public class Application {
 
