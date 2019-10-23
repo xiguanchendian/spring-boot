0582da22f177f8d5d1609b648682b50074bad895
bijiben <17600665028@163.com>
2019-10-23 14:22:45 +0800

    修改注释内容

--- src/main/java/com/xgcd/springboot/Application.java
+++ src/main/java/com/xgcd/springboot/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ImportResource;
 
 /**
  * @SpringBootApplication 该注解用于标注一个主程序类, 说明这是一个springboot应用
- * @ImportResource 导入spring的配置文件, 让配置文件里面的内容生效
+ * @ImportResource 导入spring的配置文件, 使配置文件里面的内容生效
  */
 //@ImportResource(locations = {"classpath:spring-beans.xml"})
 @SpringBootApplication
